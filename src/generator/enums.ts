5bc2dfca463c88b38158dd7eae89050ce856b8db
Marc Stammerjohann <marcstammerjohann@web.de>
2020-09-30 14:57:58 +0200

    fix(enums): test with latest dev version

--- src/generator/enums.ts
+++ src/generator/enums.ts
@@ -1,11 +1,11 @@
 import { DMMF } from '@prisma/generator-helper';
 
-export function generateEnums(enums: DMMF.Enum[]): string[] {
+export function generateEnums(enums: DMMF.DatamodelEnum[]): string[] {
   return enums.map(
     (e) => `Enum ${e.name} {\n` + generateEnumValues(e.values) + '\n}'
   );
 }
 
-const generateEnumValues = (values: string[]): string => {
-  return values.map((value) => `  ${value}`).join('\n');
+const generateEnumValues = (values: DMMF.EnumValue[]): string => {
+  return values.map((value) => `  ${value.name}`).join('\n');
 };
